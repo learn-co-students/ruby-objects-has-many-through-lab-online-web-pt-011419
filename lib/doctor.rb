bc9acfbba1fae4956c2489190c5bf112576e0eb9
Derek Cerretani <derekcerretani@gmail.com>
2019-02-13 23:39:34 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,24 @@
+class Doctor
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(patient)
+
+  end
+
+  def patients
+
+  end
+
+end
