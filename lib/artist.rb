a7ee33a3263c1cd375044069f2cb1f8c556a83a7
Nissan Holzer <nissan.holzer@gmail.com>
2019-02-18 23:53:30 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,21 @@
+class Artist 
+  
+  attr_reader :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def new_song(name, genre) 
+    
+  end 
+  
+  
+end 
