08aed8954e7aa0c43d50626e781a3a3f479552d1
Nissan Holzer <nissan.holzer@gmail.com>
2019-02-19 00:19:01 +0000

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,7 +1,22 @@
 class Genre
-  attr_reader :name 
+  attr_reader :name, :songs
+  
+  @@all = []
   
   def initialize(name)
     @name = name
+    @@all << self
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def songs 
+    Songs.all.select {|song| song.genre == self}
+  end 
+  
+  def artists
+    
   end 
 end 
