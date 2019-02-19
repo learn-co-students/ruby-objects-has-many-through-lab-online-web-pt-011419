83b735133b6d8a5366498d9e48e30a5271ef22c8
Nissan Holzer <nissan.holzer@gmail.com>
2019-02-19 00:15:58 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,12 @@
 class Artist 
   
-  attr_reader :name
+  attr_accessor :name, :songs
   
   @@all = []
   
   def initialize(name)
     @name = name
+    @songs = []
     @@all << self
   end 
   
@@ -13,9 +14,18 @@ class Artist
     @@all 
   end 
   
+  
   def new_song(name, genre) 
-    
+    song = Song.new(name, self, genre)
+    @songs << song
+    song
   end 
   
+  def songs 
+    @songs
+  end 
   
+  def genres 
+    Genre.all.select 
+  end 
 end 
